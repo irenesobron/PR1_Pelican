6ee76b8b5bb05d0a02cbc9449b3e31c68bd372af
irenesobron <147519094+irenesobron@users.noreply.github.com>
2026-02-12 21:18:28 +0100

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -8,8 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ""
-RELATIVE_URLS = False
+SITEURL = 'https://irenesobron.github.io/PR1_Pelican'
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
